7b31b05ac4e6c6bf5f399c368659afd5bb2fa9d1
ale8a <alochernandez@esimez.mx>
2019-05-30 19:25:15 -0500

    quitamos comentarios, asi como variables en objeto

--- index.js
+++ index.js
@@ -23,26 +23,25 @@ function writeJson(data) {
 //funcion principal
 function CreatJson() {
 
-    let jsonDoc = []; //variable donde se almacenara el JSON resultane
-    let doc = fs.readFileSync('./file/mentorsList.csv', 'utf8'); //lee el archivo
-    doc = doc.split("\n"); //separamos elementos por un salto de linea
-    doc.shift(); //elimina la primera fila
-    //por cada elemento se guardara en jsoDoc
+    let jsonDoc = [];
+    let doc = fs.readFileSync('./file/mentorsList.csv', 'utf8');
+    doc = doc.split("\n");
+    doc.shift();
     jsonDoc = doc.map(elem => {
-        let head = headbord(); //obtenemos las cabeceras
-        let { name, course } = head; //las guardamos en cada variable que seran las key
-        elem = elem.split(','); //cada fila, separamos sus elementos
-        let [mentorName, courseName] = elem; //el contenido de cada elemento lo guardamos en cada variable
-        let obj = {}; //constuyo un objeto para poder utilizar las variables como Key
-        obj[name] = mentorName; //se le asigna el valor a cada key								
-        obj[course] = courseName;
-        return obj; //retorna el objeto contruido
+        let head = headbord();
+        let { name, course } = head;
+        elem = elem.split(',');
+        let [mentorName, courseName] = elem;
+        return {
+            [name]: mentorName,
+            [course]: courseName
+        }
     });
-    let json = { "mentors": jsonDoc } //al obeto se le asigna una llave
-    json = JSON.stringify(json, null, 2); //convetir el objeto a formato JSON con identacion de 2 espacios
-    let res = writeJson(json); //llama la funcion para guardar nuestra informacion en un .json y espera la respuesta
-    if (res.status === 200) console.log(res.massage); //si es exitoso imprime el mensaje
-    else console.log("error en la escritura"); //si hay un error muestra mensaje de error
+    let json = { "mentors": jsonDoc }
+    json = JSON.stringify(json, null, 2);
+    let res = writeJson(json);
+    if (res.status === 200) console.log(res.massage);
+    else console.log("error en la escritura");
 }
 
 
